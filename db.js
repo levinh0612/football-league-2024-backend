7a38c6348a17af8978d57c2180595c6adced3d2b
levinhdev <vinh.nhl@fahasa.com.vn>
2024-12-24 23:59:53 +0700

    edit app.js 5

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false, // In production, you should set this to true and use proper certificates
+    }
 });
 
 module.exports = pool;
